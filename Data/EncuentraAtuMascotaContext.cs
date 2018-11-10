d1887f3052f4d8584d93ce9dd565b4d986409165
piercv <raul.camarena@usil.pe>
2018-11-10 09:52:16 -0500

    Modificaciones del 10/11

---
+++ Data/EncuentraAtuMascotaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EncuentraAtuMascota.Models
+{
+    public class EncuentraAtuMascotaContext : DbContext
+    {
+        public EncuentraAtuMascotaContext (DbContextOptions<EncuentraAtuMascotaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EncuentraAtuMascota.Models.Usuarios> Usuarios { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using EncuentraAtuMascota.Models;
+
+//using EFGetStarted.AspNetCore.NewDb.Models;
 
 namespace EncuentraAtuMascota
 {
@@ -31,8 +30,12 @@ namespace EncuentraAtuMascota
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //Conexión a BD - Adiciona el contexto que usará la aplicación
+            services.AddDbContext<EncuentraAtuMascotaContext>(options =>
+                        options.UseMySql(Configuration.GetConnectionString("Default")));
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
